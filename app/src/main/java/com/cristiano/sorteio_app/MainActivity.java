a9ae5ea6644d3e263ae770c56aecf502d3aebb0a
cristianoDevSystem <cristianodevsystem@gmail.com>
2021-01-19 22:09:39 -0300

    Projeto criado no Android Studio na linguagem nativa Java, para criar uma brincadeira de sorteio de números aletorios de 0 a 10

---
+++ app/src/main/java/com/cristiano/sorteio_app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.cristiano.sorteio_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void drawNumber(View view) {
+
+        TextView text = findViewById(R.id.textResult);
+
+        // Gerando um número aleatorio
+        int numbre = new Random().nextInt(11);
+
+        text.setText("Número sorteado foi... " + numbre);
+
+    }
+}
